48f9e13971db5c1b7f1da1a7eae8cbf7d4a98cf8
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-11-15 09:36:47 +0300

    Create Q4 power.cpp

---
+++ ETS1185_15_Salim_Aragaw/Q4 power.cpp
@@ -0,0 +1,14 @@
+//Power Function Calculator
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+float X,Y;
+    cout<<"Please Enter the First Number"<< endl;
+    cin>>X;
+    cout<<"Please Enter the Second Number"<< endl;
+    cin>>Y;
+    cout<<pow(X,Y)<< endl;
+    return 0;
+}
