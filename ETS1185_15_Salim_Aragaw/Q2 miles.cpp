f4c93e52774899804b69e8d41a43f6bd1433d541
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-11-15 09:24:30 +0300

    Create Q2 miles.cpp

---
+++ ETS1185_15_Salim_Aragaw/Q2 miles.cpp
@@ -0,0 +1,18 @@
+//Miles on a Full Tank Calculator
+#include<iostream>
+#include<ctype.h>
+#include<math.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    float Fuel_Tank_Capacity, Fuel_Consumption, Miles_on_Full_Tank;
+    cout<<"Enter The Fuel Tank Capacity of The Automobile in Gallons"<< endl;
+    cin>>Fuel_Tank_Capacity;
+    cout<<"Enter the Fuel Consumption in Miles per Gallon"<< endl;
+    cin>>Fuel_Consumption;
+    Miles_on_Full_Tank = Fuel_Consumption*Fuel_Tank_Capacity;
+    cout<<"The Max Miles on Full Tank of this car is"<< endl;
+    cout<<Miles_on_Full_Tank<<" Miles"<< endl;
+    return 0;
+}
