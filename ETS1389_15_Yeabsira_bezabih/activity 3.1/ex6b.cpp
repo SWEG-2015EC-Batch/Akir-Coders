404966cce614b9140c8e751a585fe69b7aaacddf
Yeabdevts <149267151+Yeabdevts@users.noreply.github.com>
2023-12-17 23:05:07 +0300

    ex6b.cpp

--- ETS1389_15_Yeabsira_bezabih/activity 3.1/ex6b.cpp
+++ ETS1389_15_Yeabsira_bezabih/activity 3.1/ex6b.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int count = 0,fnum,lnum;
+    cout <<"Enter the first number: ";
+    cin>>fnum;
+    cout <<"Enter the last  number: ";
+    cin>>lnum;
 
+    for (int i = fnum; i <= lnum; i++)
+    {
+        cout << i << " ";
+        count++;
+
+        if (count % 10 == 0)
+        {
+        cout<<endl;
+        }
+    }
+
+    return 0;
+}
