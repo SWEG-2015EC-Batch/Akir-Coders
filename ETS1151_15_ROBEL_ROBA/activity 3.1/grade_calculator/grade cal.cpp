fa635bba31ca2d0944b01a6ad8f50e620759a3e7
RobelRoba <149502927+RobelRoba@users.noreply.github.com>
2023-12-17 22:14:59 +0300

    Update grade cal.cpp

--- ETS1151_15_ROBEL_ROBA/activity 3.1/grade_calculator/grade cal.cpp
+++ ETS1151_15_ROBEL_ROBA/activity 3.1/grade_calculator/grade cal.cpp
@@ -1,1 +1,74 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int test , quiz , project, assignment , finalexam , total;
+ cout<<"enter your test mark from 15%: ";
+ cin>>test;
+ cout<<"enter your quiz mark from 5%: ";
+ cin>>quiz;
+ cout<<"enter your project mark from 20%: ";
+ cin>>project;
+ cout<<"enter your assignment mark from 10%: ";
+ cin>>assignment;
+ cout<<"enter your final exam mark from 50%: ";
+ cin>>finalexam;
+ 
+    total = test + quiz + project + assignment + finalexam;
+    if((total<0)||(total>100))
+    {
+        cout<<"invald input";
+        
+    }
+    else
+    {
+    cout<<"your total mark is "<<total;
 
+    }
+    
+if (total>=90)
+{
+    cout<<"your grade is A+";
+}
+else if (total>=80)
+{
+    cout<<"your grade is A";
+}
+else if (total>=75)
+{
+    cout<<"your grade is B+";
+}
+
+else if (total>=60)
+{
+    cout<<"your grade is B";
+}
+
+else if (total>=55)
+{
+    cout<<"your grade is C+";
+}
+
+else if (total>=45)
+{
+    cout<<"your grade is C";
+}
+
+else if (total>=30)
+{
+    cout<<"your grade is D";
+}
+else if (total<30)
+{
+    cout<<"your grade is F";
+}
+else
+{
+  cout<<"unknown grade";
+  
+}
+
+
+return 0;
+
+}
