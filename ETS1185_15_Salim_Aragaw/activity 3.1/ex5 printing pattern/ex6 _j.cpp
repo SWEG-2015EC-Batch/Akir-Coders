13e314f7f6874c6b2607ec09824237a65d4669c0
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-12-18 18:06:32 +0300

    Create ex6 _j.cpp

---
+++ ETS1185_15_Salim_Aragaw/activity 3.1/ex5 printing pattern/ex6 _j.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num=5;
+
+for (int i = 1; i <= num; i++)
+{
+    for (int j = i; j <=5; j++)
+    {
+        cout<<j;
+    }
+    cout<<endl;
+}
+return 0;
+}
