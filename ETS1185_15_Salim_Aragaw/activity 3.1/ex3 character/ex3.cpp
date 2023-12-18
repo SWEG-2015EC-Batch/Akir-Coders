e3bcc360a51c3e883fcd5db2383d2c1a7d214f5d
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-12-18 17:29:24 +0300

    Create ex3.cpp

---
+++ ETS1185_15_Salim_Aragaw/activity 3.1/ex3 character/ex3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char character;
+    cout << "Enter a character: ";
+    cin >> character;
+
+    if (character >= 'A' && character <= 'Z') {
+        cout << "Uppercase letter" << endl;
+        if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U')
+            cout << "It's a vowel" << endl;
+        else
+            cout << "It's a consonant" << endl;
+    }
+    else if (character >= 'a' && character <= 'z') {
+        cout << "Lowercase letter" << endl;
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            cout << "It's a vowel" << endl;
+        else
+            cout << "It's a consonant" << endl;
+    }
+    else if (character >= '0' && character <= '9') {
+        cout << "Digit" << endl;
+        int num = character - '0';
+        if (num % 2 == 0)
+            cout << "It's an even number" << endl;
+        else
+            cout << "It's an odd number" << endl;
+    } 
+    else {
+        cout << "Special character" << endl;
+    }
+
+    return 0;
+}
