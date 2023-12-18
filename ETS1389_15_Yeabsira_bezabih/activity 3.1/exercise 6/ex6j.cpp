05d47bacabf37d51183ec77ff6ccc4530b33ac88
Yeabdevts <149267151+Yeabdevts@users.noreply.github.com>
2023-12-18 21:19:47 +0300

    Add files via upload

---
+++ ETS1389_15_Yeabsira_bezabih/activity 3.1/exercise 6/ex6j.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num=5;
+
+for (int i = 1; i < num; i++)
+{
+    for (int j = 1; j <=5; j++)
+    {
+        cout<<j;
+    }
+    cout<<endl;
+}
+
+
+
+}
