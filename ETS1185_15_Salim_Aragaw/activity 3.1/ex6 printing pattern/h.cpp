4513ac0129b03b1d13e1118ad66c10a3112fc789
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-12-18 18:18:36 +0300

    Create h.cpp

---
+++ ETS1185_15_Salim_Aragaw/activity 3.1/ex6 printing pattern/h.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows;
+   
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+   
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= rows - i; ++j) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            if (j == 1 || j == 2 * i - 1 || i == rows)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
