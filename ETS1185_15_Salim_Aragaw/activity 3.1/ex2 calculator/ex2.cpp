1d95f79d5f5fc2f700bf0271580a5328f7e244eb
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2023-12-18 17:28:03 +0300

    Create ex2.cpp

---
+++ ETS1185_15_Salim_Aragaw/activity 3.1/ex2 calculator/ex2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    int n1,n2,n3,exit;
+      do
+    {
+    cout<<"enter the first number: ";
+    cin>>n1;
+      cout<<"enter the operation: ";
+    cin>>c;
+      cout<<"enter the second number: ";
+    cin>>n2;
+  
+    
+    switch (c)
+    {
+    case '+':
+        n3=n1+n2;
+        cout<<"The sum of the numbers is "<<n3<<endl;
+         break;
+     case '-':
+        n3=n1-n2;
+        cout<<"The difference of the numbers is "<<n3<<endl;
+        break;
+     case '*':
+        n3=n1*n2;
+        cout<<"The produce of the two numbers is "<<n3<<endl;
+        break;
+     case '/':
+        n3=n1/n2;
+        cout<<"The quoitent of the two numbers is "<<n3<<endl;
+        break;
+     case '%':
+        n3=n1%n2;
+        cout<<"The reminder of the two number is "<<n3<<endl;
+        break;
+    
+    default:
+        cout<<"invalid operator";
+        break;
+    }
+    cout<<"do youn want to continue? yes[any number] no[0]:";
+    cin>>exit;
+    } while (exit!=0);
+ 
+return 0;
+} 
