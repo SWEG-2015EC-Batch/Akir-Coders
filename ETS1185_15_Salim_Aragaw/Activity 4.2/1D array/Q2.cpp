c8deb543b359c05de3185f2efb099a73305e5594
Salimjr7 <149458119+Salimjr7@users.noreply.github.com>
2024-01-14 22:22:15 +0300

    Create Q2.cpp

---
+++ ETS1185_15_Salim_Aragaw/Activity 4.2/1D array/Q2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int temp[8];
+    int sum=0,count=0,avg;
+    for (int i = 0; i < 8; i++)
+    {
+        int j = i+1;
+        cout<< "Enter number "<<j<<":";
+        cin>>temp[i];
+        sum+=temp[i];
+        count++;
+
+    }
+    avg = sum/count;
+    cout<<"The sum of tne numbers is "<<sum<<endl;
+    cout<<"The average of the numbers is "<<avg;
+
+    return 0;
+}
