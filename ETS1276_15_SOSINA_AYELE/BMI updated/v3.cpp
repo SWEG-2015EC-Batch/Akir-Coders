6ad8006dbe900eabe8e41f0502f38c0d157fcf05
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-01-27 23:29:11 +0300

    Create v3.cpp

---
+++ ETS1276_15_SOSINA_AYELE/BMI updated/v3.cpp
@@ -0,0 +1,48 @@
+//Body Mass Index Calculator
+#include<iostream>
+using namespace std;
+int main()
+{
+    float Height, Weight, BMI, number;
+    string gender, male, female;
+    cout<<"How many people BMI do you want to calculate =";
+    cin>>number;
+    for (float i = 0; i < number;i++)
+    {
+        
+     cout<<"Please Enter your Height"<< endl;
+    cin>>Height;
+    cout<<"Please Enter your Weight"<< endl;
+    cin>>Weight;
+    cout<<"Enter your gender say male or female"<<endl;
+    cin>>gender;
+    BMI = Weight/(Height*Height);
+    cout<<"Your BMI is: "<<BMI<<endl;
+    
+    if(gender=="female"){
+    
+    if (BMI < 18.5) {
+        cout << "female Underweight" << std::endl;
+    } else if (BMI >= 18.5 && BMI <= 24) {
+        cout << "female Normal Weight" << std::endl;
+    } else if (BMI > 24 && BMI <= 30) {
+        cout << "female Overweight" <<endl;
+    } else {
+        cout << "female Obesity" <<endl;
+    }
+    }
+  
+  if (gender=="male")  {
+if (BMI < 18.5) {
+        cout << "male Underweight" <<endl;
+    } else if (BMI >= 18.5 && BMI <= 24) {
+        cout << "male Normal Weight" <<endl;
+    } else if (BMI > 24 && BMI <= 30) {
+        cout << "male Overweight" <<endl;
+    } else {
+        cout << "male Obesity" <<endl;
+    }
+  }
+    }
+    return 0;
+}
